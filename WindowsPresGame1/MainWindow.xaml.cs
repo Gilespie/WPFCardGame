77c979c3aefbab8fbaa7b66838e3a52fb16cffed
Gilespie <pierreraider@gmail.com>
2024-12-19 03:10:34 -0300

    Added timer and replay. Reduced count of cards.

--- WindowsPresGame1/MainWindow.xaml.cs
+++ WindowsPresGame1/MainWindow.xaml.cs
@@ -15,15 +15,35 @@ using System.Windows.Shapes;
 
 namespace WindowsPresGame1
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            TimerText.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                TimerText.Text = TimerText.Text + " - Play again?";
+            }
         }
 
         private void SetUpGame()
@@ -32,8 +52,6 @@ namespace WindowsPresGame1
             {
                 "🦎","🦎",
                 "🦨","🦨",
-                "🦈","🦈",
-                "🐙","🐙",
                 "🦁","🦁",
                 "🐸","🐸",
                 "🐦","🐦",
@@ -46,10 +64,52 @@ namespace WindowsPresGame1
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = ramdom.Next(animals.Count);
-                string nextEmoji = animals[index];
-                textBlock.Text = nextEmoji;
-                animals.RemoveAt(index);
+                if (textBlock.Name != "TimerText")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = ramdom.Next(animals.Count);
+                    string nextEmoji = animals[index];
+                    textBlock.Text = nextEmoji;
+                    animals.RemoveAt(index);
+                }
+            }
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+
+            if(findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if(textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimerText_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(matchesFound == 8)
+            {
+                SetUpGame();
             }
         }
     }
